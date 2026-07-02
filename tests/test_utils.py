0871722aa7b711d1f281a5cf05d67b1c0fb8aa83
mgrasshoff <martin.grasshoff@protonmail.com>
2026-07-02 11:58:37 +0200

    Added second test for _utils (_build_consensus)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,19 @@
 # Tests for the functions in the _utils module.
 
+import warnings
+
 import anndata as ad
 import numpy as np
 import scipy.sparse as sp
 
 from spatial_sigurd_py._utils import _to_array
 
+# Since _build_consensus is unused for now, we have to import it directly.
+from spatial_sigurd_py._utils.basic import _build_consensus
+
+###############################################################################
+# Testing _to_array
+###############################################################################
 
 # Function to make an AnnData object. This will be used in the function
 # test_sparse_layer_returns_transposed_dense_float32
@@ -53,3 +61,67 @@ def test_sparse_and_dense_paths_agree():
         _to_array("counts", _make_adata(dense)),
         _to_array("counts", _make_adata(sp.csr_matrix(dense))),
     )
+
+
+###############################################################################
+# Testing _build_consensus
+###############################################################################
+def _codes():
+    # cov >= alt everywhere (coverage = alt + ref)
+    # (0,0) cov=0 alt=0 -> 0 NoCall
+    # (0,1) cov=5 alt=0 -> 1 Reference
+    # (1,0) cov=5 alt=3 -> 2 Variant (partial alt)
+    # (1,1) cov=8 alt=8 -> 2 Variant (all alt reads)
+    cov = sp.csr_matrix(np.array([[0, 5], [5, 8]], dtype = np.int32))
+    alt = sp.csr_matrix(np.array([[0, 0], [3, 8]], dtype = np.int32))
+    return alt, cov
+
+
+def test_all_three_codes_and_variant_precedence():
+    alt, cov = _codes()
+    out = _build_consensus(alt, cov)
+    np.testing.assert_array_equal(out.toarray(), np.array([[0, 1], [2, 2]]))
+
+
+def test_shape_and_orientation_preserved():
+    alt, cov = _codes()
+    out = _build_consensus(alt, cov)
+    assert out.shape == cov.shape    # (n_cells, n_variants), no transpose
+
+
+def test_output_stays_sparse():
+    alt, cov = _codes()
+    assert sp.issparse(_build_consensus(alt, cov))
+
+
+def test_nocall_positions_not_stored():
+    alt, cov = _codes()
+    out = _build_consensus(alt, cov)
+    assert out.nnz == 3               # the single 0 (NoCall) is dropped
+    assert (out.data == 0).sum() == 0 # eliminate_zeros actually ran
+
+
+def test_default_dtype_is_int8():
+    alt, cov = _codes()
+    assert _build_consensus(alt, cov).dtype == np.int8
+
+
+def test_custom_dtype_respected():
+    alt, cov = _codes()
+    assert _build_consensus(alt, cov, dtype = np.int16).dtype == np.int16
+
+
+def test_stored_explicit_zero_in_alt_is_not_a_variant():
+    # alt has a *stored* zero at (0,0); it must not be coded as a variant
+    alt = sp.csr_matrix(
+        (np.array([0, 3]), (np.array([0, 1]), np.array([0, 0]))), shape=(2, 1)
+    )
+    cov = sp.csr_matrix(np.array([[7], [7]], dtype=np.int32))
+    assert _build_consensus(alt, cov).toarray()[0, 0] == 1   # Reference, not Variant
+
+
+def test_no_sparse_efficiency_warning():
+    alt, cov = _codes()
+    with warnings.catch_warnings():
+        warnings.simplefilter("error")
+        _build_consensus(alt, cov)
